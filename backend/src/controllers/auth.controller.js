7f42b0c8362f5771ab3fb89734ea912a1bca6169
Ashadeep Pan <ashadeeppan18@gmail.com>
2026-03-21 17:42:35 +0530

    Updated cookie settings for production

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -30,7 +30,11 @@ async function registerUser(req, res) {
       process.env.JWT_SECRET
     );
 
-    res.cookie("token", token);
+    res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none"
+});
 
     res.status(201).json({
       message: "User registered successfully",
@@ -76,7 +80,11 @@ async function loginUser(req, res) {
         id: user._id,
     }, process.env.JWT_SECRET)
 
-    res.cookie("token", token)
+    res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none"
+});
 
     res.status(200).json({
         message: "User logged in successfully",
@@ -124,7 +132,11 @@ async function registerFoodPartner( req, res ){
     id: foodPartner._id,
   }, process.env.JWT_SECRET)
 
-  res.cookie("token", token)
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none"
+});
 
   res.status(201).json({
     message: "Food partner registered successfully",
